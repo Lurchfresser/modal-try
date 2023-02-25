72fd57f67e1123aacf6ad345e5e513e73b3cbe26
Lurchfresser <Tritrop007@gmx.de>
2023-02-25 14:32:31 +0100

    code cleanup, fixed recursionbug at startModal()

--- modal/modal.js
+++ modal/modal.js
@@ -36,59 +36,43 @@ function append() {
 let mouseX = 0;
 let mouseY = 0;
 
-document.querySelector("body").onmousemove = (event) => {
-    mouseX = event.clientX;
-    mouseY = event.clientY;
-}
-document.addEventListener("keydown", async (key) => {
-    if ((key.key == "m" || key.key == "M") && key.ctrlKey) {
-        if (activated) {
-            await hideModal()
-        }
-        else if (!preActivated){
-            preActivated = true;
-        }
-    } else if (key.key == "Escape" && activated) {
-        await hideModal();
-    } else if (key.key == "Enter" && activated) {
-        await confirmChoice();
-    }
-});
-
-
 let reviewCardContainerSet = new Set;
 //check if the review containing div has been loaded
-let loadObserver = new MutationObserver(function(mutations) {
-    if (document.getElementsByClassName("maingrid__content content").length !== 0){
+let loadObserver = new MutationObserver(function (mutations) {
+    if (document.getElementsByClassName("maingrid__content content").length !== 0) {
     }
-        //mit added Note vielleicht performanter
-    if (document.getElementsByClassName("content__body__main reviews-content-body").length !== 0){
+    //mit added Note vielleicht performanter
+    if (document.getElementsByClassName("content__body__main reviews-content-body").length !== 0) {
         reviewContentBody = document.getElementsByClassName("content__body__main reviews-content-body")[0];
         reviewObserver.observe(reviewContentBody,
-            {childList:true,subtree:true});
+            {childList: true, subtree: true});
         loadObserver.disconnect();
     }
 });
-loadObserver.observe(document.documentElement,{childList:true,subtree:true});
+loadObserver.observe(document.documentElement, {childList: true, subtree: true});
 let reviewContentBody;
-let reviewObserver = new MutationObserver(function(mutations) {
-    for (let mutation of mutations){
-        if (mutation.addedNodes.length >= 0 || mutation.removedNodes.length >= 0){
-            for (let addedNote of mutation.addedNodes){
-                if (addedNote.className === "reviewcard__starrating"){
+let reviewObserver = new MutationObserver(function (mutations) {
+    for (let mutation of mutations) {
+        if (mutation.addedNodes.length >= 0 || mutation.removedNodes.length >= 0) {
+            for (let addedNote of mutation.addedNodes) {
+                if (addedNote.className === "reviewcard__starrating") {
                     reviewCardContainerSet.add(addedNote.parentElement.parentElement.parentElement);
-                    addedNote.parentElement.parentElement.parentElement.onmouseenter = (e)=>{selectReviewcard(e)};
-                    addedNote.parentElement.parentElement.parentElement.onmouseleave = (e)=>{deSelectReviewcard(e)};
+                    addedNote.parentElement.parentElement.parentElement.onmouseenter = (e) => {
+                        selectReviewcard(e)
+                    };
+                    addedNote.parentElement.parentElement.parentElement.onmouseleave = (e) => {
+                        deSelectReviewcard(e)
+                    };
                 }
             }
         }
-        if (mutation.removedNodes.length >= 0){
-            for (let removedNode of mutation.removedNodes){
+        if (mutation.removedNodes.length >= 0) {
+            for (let removedNode of mutation.removedNodes) {
                 //können bei Antworten auch andere Divs removed werden
-                if (removedNode.tagName === "DIV" && removedNode?.firstElementChild?.firstElementChild?.firstElementChild.className === "reviewcard__container"){
+                if (removedNode.tagName === "DIV" && removedNode?.firstElementChild?.firstElementChild?.firstElementChild.className === "reviewcard__container") {
                     console.log("yay");
-                    removedNode.firstElementChild.firstElementChild.firstElementChild.removeEventListener("onmouseenter",selectReviewcard);
-                    removedNode.firstElementChild.firstElementChild.firstElementChild.removeEventListener("onmouseleave",deSelectReviewcard);
+                    removedNode.firstElementChild.firstElementChild.firstElementChild.removeEventListener("onmouseenter", selectReviewcard);
+                    removedNode.firstElementChild.firstElementChild.firstElementChild.removeEventListener("onmouseleave", deSelectReviewcard);
                     reviewCardContainerSet.delete(removedNode.firstElementChild.firstElementChild.firstElementChild);
                 }
             }
@@ -98,34 +82,40 @@ let reviewObserver = new MutationObserver(function(mutations) {
 
 let selectedReviewcard;
 
-function selectReviewcard(e){
+function selectReviewcard(e) {
     if (preActivated) {
         selectedReviewcard = e.target;
         e.target.style.border = "1px solid black";
         //ist glaube ich nicht ganz der korrekte borderRadius
         e.target.style.borderRadius = "5px";
-
-        e.target.addEventListener("click",startModal);
+        e.target.addEventListener("click", startModal);
     }
 }
-function deSelectReviewcard(e){
-    if (selectedReviewcard === e.target){
+
+function deSelectReviewcard(e) {
+    if (selectedReviewcard === e.target) {
         selectedReviewcard = undefined;
     }
     e.target.style.borderWidth = "0px";
 
-    e.target.removeEventListener("click",startModal);
+    e.target.removeEventListener("click", startModal);
 }
 
 //cancelbutton implementieren und Antwort abschicken
-async function startModal(e){
-    for (let Element of this.getElementsByClassName("reviewcard__reviewfooter__reply")){
-        if (Element.classList.length === 1){
+async function startModal(e) {
+    console.log(e.target);
+    //for loop because the hidden buttons also have the same classname(but not same classlist -> if(classlist.lenght))
+    for (let Element of this.getElementsByClassName("reviewcard__reviewfooter__reply")) {
+        if (Element.classList.length === 1) {
+            //remove and add Eventlistener, because the clickfunction would trigger the function recursivly
+            this.removeEventListener("click", startModal);
             Element.click();
+            this.addEventListener("click", startModal);
+            //textarea needs 1 milisec to be loaded
             let temp;
             while (!temp) {
                 await sleep(1);
-                if (this.querySelector("textarea")){
+                if (this.querySelector("textarea")) {
                     temp = true;
                 }
             }
@@ -136,43 +126,31 @@ async function startModal(e){
             break;
         }
     }
-    for (let Element2 of this.getElementsByClassName("button is-smaller is-outlined-primary")){
-        if (Element2.classList.length === 3){
-            Element2.click();
-            let temp2;
-            while (!temp2) {
-                await sleep(1);
-                if (this.querySelector("textarea")){
-                    temp2 = true;
-                }
-            }
+    //if is for the cancelbutton to work
+    if (e.target.className !== "button is-smaller u-mr-s") {
+        for (let Element2 of this.getElementsByClassName("button is-smaller is-outlined-primary")) {
+            if (Element2.classList.length === 3) {
+                this.removeEventListener("click", startModal);
+                Element2.click();
+                this.addEventListener("click", startModal);
+                let temp2;
+                while (!temp2) {
+                    await sleep(1);
+                    if (this.querySelector("textarea")) {
+                        temp2 = true;
+                    }
 
-            replyTextArea = this.querySelector("textarea");
-            replyTextArea.value = "Hallo " + replyTextArea.value;
-            await showModal();
-            break;
+                }
+                replyTextArea = this.querySelector("textarea");
+                replyTextArea.value = "Hallo " + replyTextArea.value;
+                await showModal();
+                break;
+            }
         }
     }
 }
 
-function sleep(ms){
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
-window.onkeydown = function (e) {
-    if (activated) {
-        return !(e.key == "Spacebar" || e.key == " " || e.key == "Enter");
-    }
-};
-
-
-async function lockChangeAlert() {
-    if (!(document.pointerLockElement === modal) && activated) {
-        //await hideModal();
-    }
-}
-
-document.addEventListener("pointerlockchange", lockChangeAlert, false);
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API#simple_example_walkthrough
 // anschauen und in finaler Version besser machen
@@ -180,7 +158,7 @@ async function showModal() {
     modalSelected = 4;
     lastSelectedX = [];
     lastSelectedY = [];
-    modalClass.show(mouseX-150,mouseY-150,modalSelected);
+    modalClass.show(mouseX - 150, mouseY - 150, modalSelected);
     activated = true;
     //firefox akzeptiert nicht, später ändern
     await modal.requestPointerLock();
@@ -238,12 +216,12 @@ function select(e) {
 
 
 let replyTextArea;
+
 async function confirmChoice() {
     let tab = template.tabs[modalClass.divToTemplate(modalSelected)];
-    if (tab["depthlevel"] !== 2){
+    if (tab["depthlevel"] !== 2) {
         replyTextArea.value += modalClass.output(tab);
-    }
-    else if (tab["depthlevel"] !== 2){
+    } else if (tab["depthlevel"] !== 2) {
         template = tab;
         await showModal();
         modalClass.showChoice(template);
@@ -266,18 +244,53 @@ Math.degrees = function (radians) {
 function average(arr) {
     return arr.reduce((a, b) => a + b, 0) / arr.length;
 }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+
+async function lockChangeAlert() {
+    if (!(document.pointerLockElement === modal) && activated) {
+        //await hideModal();
+    }
+}
+
+document.addEventListener("pointerlockchange", lockChangeAlert, false);
 document.addEventListener("mousemove", (e) => {
     if (document.pointerLockElement === modal) {
         select(e);
     }
 })
+document.querySelector("body").onmousemove = (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+}
+document.addEventListener("keydown", async (key) => {
+    if ((key.key == "m" || key.key == "M") && key.ctrlKey) {
+        if (activated) {
+            await hideModal()
+        } else if (preActivated) {
+            preActivated = false;
+        } else if (!preActivated) {
+            preActivated = true;
+        }
+    } else if (key.key == "Escape" && activated) {
+        await hideModal();
+    } else if (key.key == "Enter" && activated) {
+        await confirmChoice();
+    }
+});
+
+window.onkeydown = function (e) {
+    if (activated) {
+        return !(e.key == "Spacebar" || e.key == " " || e.key == "Enter");
+    }
+};
 
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
     if (request?.reason === "updated") {
         await Start();
-    }
-    else if (request?.reason === "new template") {
+    } else if (request?.reason === "new template") {
 
     } else if (request?.reason === "hideNav") {
         NavIsShown = !request.checked;
